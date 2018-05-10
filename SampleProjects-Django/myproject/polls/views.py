921b1a2f8c27ddddc1fa0e6ab66353094f9a6ab3
sinhaG <grv.sinha@gmail.com>
2018-05-10 18:19:47 +0530

    Geeneric views

--- SampleProjects-Django/myproject/polls/views.py
+++ SampleProjects-Django/myproject/polls/views.py
@@ -15,9 +15,27 @@ from django.shortcuts import get_object_or_404,render
 from .models import Question,Choice
 from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponse
-
+from django.views import generic
 from django.db.models import F
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+"""     
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -27,19 +45,19 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-""" def detail(request, question_id):
+"" def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     
-    return render(request, 'polls/detail.html', {'question': question}) """
+    return render(request, 'polls/detail.html', {'question': question}) ""
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+ """
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
