f4b2d4c29f1d73739fa78d64f692c033cf3b4be3
sinhaG <grv.sinha@gmail.com>
2018-05-15 10:11:09 +0530

    sas

--- SampleProjects-Django/myproject/myproject/urls.py
+++ SampleProjects-Django/myproject/myproject/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     
+     
+    
 ]
--- SampleProjects-Django/myproject/polls/admin.py
+++ SampleProjects-Django/myproject/polls/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Question,Choice
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
-admin.site.register(Question)
 
-admin.site.register(Choice)
-# Register your models here.
+
+admin.site.register(Question,QuestionAdmin)
+
+
